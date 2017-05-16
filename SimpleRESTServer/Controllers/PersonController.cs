6f0bce85f7e01edfb3a79f49ad34a5560d78129c
almir <almir.garic.1992@gmail.com>
2017-05-16 18:40:04 +0200

    First commit

---
+++ SimpleRESTServer/Controllers/PersonController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SimpleRESTServer.Models;
+
+namespace SimpleRESTServer.Controllers
+{
+    public class PersonController : ApiController
+    {
+        // GET: api/Person
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Person1", "Person2" };
+        }
+
+        // GET: api/Person/5
+        public Person Get(int id)
+        {
+            Person osoba = new Person();
+
+            osoba.ID = 1;
+            osoba.FirstName = "Almir";
+            osoba.LastName = "Garic";
+            osoba.PayRate = 10000;
+            osoba.StartDate =DateTime.Parse("06/01/2017");
+            osoba.EndDate = DateTime.Parse("06/01/2018");
+
+            return osoba;
+        }
+
+        // POST: api/Person
+        public void Post([FromBody]Person value)
+        {
+            PersonPersistence pp = new PersonPersistence();
+            long id;
+            id = pp.SavePerson(value);
+        }
+
+        // PUT: api/Person/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Person/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SimpleRESTServer/PersonPersistence.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SimpleRESTServer.Models;
+using MySql.Data;
+
+namespace SimpleRESTServer
+{
+    public class PersonPersistence
+    {
+        private MySql.Data.MySqlClient.MySqlConnection conn;
+       
+
+        public PersonPersistence()
+        {
+
+           string  myConnectionString = "Server=127.0.0.1;Port=3306;Database=employeedb;Uid=root;Pwd=''";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                // MessageBox.Show(ex.Message);
+            }
+                
+        }
+
+        public long SavePerson(Person PersonToSave)
+        {
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand("INSERT INTO TABLE Personnel (FirstName,LastName,PayRate,StartDate,EndDate) VALUES('" + PersonToSave.FirstName + "','" + PersonToSave.LastName + "','" + PersonToSave.PayRate + "','" + PersonToSave.StartDate.ToString("yyyy-MM-dd HH:mm:ss") + "','" + PersonToSave.EndDate.ToString("yyyy-MM-dd HH:mm:ss") + "')", conn);
+            cmd.ExecuteNonQuery();
+            long id = cmd.LastInsertedId;
+            return id;
+        }
+    }
+}
